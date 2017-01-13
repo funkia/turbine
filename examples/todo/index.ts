bc3f26af5f150af8ce452d38a3ffd915ac1505c2
Emil Gjørup <limemloh@gmail.com>
2017-01-13 15:26:24 +0100

    todo example: remove todos

--- examples/todo/index.ts
+++ examples/todo/index.ts
@@ -2,7 +2,7 @@ import {sequence} from "jabz/traversable";
 import {
   Behavior, scan,
   Now, sample,
-  Stream, scanS
+  Stream, scanS, combine
 } from "hareactive";
 import {runMain, component, elements} from "../../src";
 const {h1, p, header, footer, section} = elements;
@@ -12,17 +12,22 @@ import todoList, {Params as ListParams, Out as ListOut} from "./src/TodoList";
 import {Params as ItemParams} from "./src/Item";
 import todoFooter, {Params as FooterParams} from "./src/TodoFooter";
 
-const concat = <A>(a: A[], b: A[]): A[] => [].concat(b, a);
+const concat = <A>(a: A[], b: A[]): A[] => [].concat(a, b);
+const apply = <A>(f: (a: A) => A, a: A) => f(a);
 
-const toItemParams = (name: string, items: ItemParams[]) => concat(items, [{
-  id: items[0] == undefined ? 0 : items[0].id + 1,
+const toItemParams = (name: string, prev: ItemParams) => ({
+  id: prev.id + 1,
   name
-}]);
+});
 
 type FromView = ListOut & InputOut;
 
 function* model({enterTodoS, toggleAll, deleteS}: FromView) {
-  const todoNames: Behavior<any> = yield sample(scan(toItemParams, [], enterTodoS));
+  const newTodoS: Stream<ItemParams> = yield sample(scanS(toItemParams, {id: 0}, enterTodoS));
+  const prependNewTodoS = newTodoS.map((todo) => (list: ItemParams[]) => concat([todo], list));
+  const removeTodoS = deleteS.map((removeId) => (list: ItemParams[]) => list.filter(({id}) => id !== removeId));
+  const modifications = combine(prependNewTodoS, removeTodoS);
+  const todoNames: Behavior<ItemParams[]> = yield sample(scan(apply, [], modifications));
   return [{todoNames}, {}];
 }
 
