e3c9f3bd75c579068742bea466006645ee1ee4fb
dmitriz <dmitri14@gmail.com>
2017-05-19 13:35:51 +0100

    Finish hello world example

--- examples/hello-world/index.ts
+++ examples/hello-world/index.ts
@@ -1,11 +1,27 @@
-import {map, go} from "@funkia/jabz"
-import {runComponent, elements} from "../../src"
-const {label, input, div} = elements
+import { runComponent, elements } from "../../src"
+const { label, span, input, div } = elements
 
-function* main() {
-  yield label('Please enter your name:')
-  // yield input()
-  return input()
+// declare your main component as generator function,
+// components from all yield expression will be combined in array of dom elements
+// and displayed when the main component is run
+const main = function* () {
+
+  // will produce <div>Welcome from Turbine</div>
+  yield div('Welcome from Turbine!')
+
+  // will produce <input placehoder="Your name?" autofocus="true"> 
+  // will store the behavior of the input values in the 'name' variable
+  const { inputValue: name } = yield input({
+    attrs: {
+      autofocus: "true", 
+      placeholder: "Your name?"
+    }
+  })
+
+  // will produce <div>Hello, __</div> 
+  // with __ replaced by the current value of 'name'
+  // the 'name' will instantly update as user types in the <input> field
+  yield div(['Hello, ', name])
 }
 
 // `runMain` should be the only impure function in application code
