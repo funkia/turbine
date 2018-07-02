6ffd2c82d4f82d6c57412bf6bf53ba0de9c042a3
Simon Friis Vindum <simonfv@gmail.com>
2018-07-02 15:24:16 +0200

    Tweak fahrenheit example

--- examples/fahrenheit-celsius/index.ts
+++ examples/fahrenheit-celsius/index.ts
@@ -1,41 +1,55 @@
-import { Behavior, stepper, Stream, combine, sample } from "@funkia/hareactive";
+import { Behavior, combine, sample, stepper, Stream } from "@funkia/hareactive";
+import { elements, fgo, modelView, runComponent, toComponent } from "../../src";
 
-import { runComponent, elements, loop, modelView, toComponent, fgo } from "../../src";
 const { input, div, label } = elements;
 
 type Model = {
-  celsius: Behavior<number>,
-  fahren: Behavior<number>
+  celsius: Behavior<number>;
+  fahren: Behavior<number>;
 };
 
 type View = {
-  fahrenChange: Stream<string>,
-  celsiusChange: Stream<string>
+  fahrenChange: Stream<string>;
+  celsiusChange: Stream<string>;
 };
 
 const getValue = (ev: any) => ev.currentTarget.value;
 
-const model = fgo(function* ({ fahrenChange, celsiusChange }: View) {
-  const fahrenNrChange = fahrenChange.map(parseFloat).filter((n) => !isNaN(n));
-  const celsiusNrChange = celsiusChange.map(parseFloat).filter((n) => !isNaN(n));
-  const celsius = yield sample(stepper(0, combine(celsiusNrChange, fahrenNrChange.map((f) => (f - 32) / 1.8))));
-  const fahren = yield sample(stepper(0, combine(fahrenNrChange, celsiusNrChange.map((c) => c * 9 / 5 + 32))));
+const parseNumbers = (s: Stream<string>) =>
+  s.map(parseFloat).filter((n) => !isNaN(n));
+
+const model = fgo(function*({ fahrenChange, celsiusChange }: View) {
+  const fahrenNrChange = parseNumbers(fahrenChange);
+  const celsiusNrChange = parseNumbers(celsiusChange);
+  const celsius = yield sample(
+    stepper(
+      0,
+      combine(celsiusNrChange, fahrenNrChange.map((f) => (f - 32) / 1.8))
+    )
+  );
+  const fahren = yield sample(
+    stepper(
+      0,
+      combine(fahrenNrChange, celsiusNrChange.map((c) => (c * 9) / 5 + 32))
+    )
+  );
   return { celsius, fahren };
 });
 
-const view = ({ fahren, celsius }) => div([
+const view = ({ fahren, celsius }: Model) =>
   div([
-    label("Fahrenheit"),
-    input({ props: { value: fahren }, output: { fahrenInput: "input" } })
-  ]),
-  div([
-    label("Celsius"),
-    input({ props: { value: celsius }, output: { celsiusInput: "input" } })
-  ])
-]).map(({ fahrenInput, celsiusInput }) => ({
-  fahrenChange: fahrenInput.map(getValue),
-  celsiusChange: celsiusInput.map(getValue)
-}));
+    div([
+      label("Fahrenheit"),
+      input({ props: { value: fahren }, output: { fahrenInput: "input" } })
+    ]),
+    div([
+      label("Celsius"),
+      input({ props: { value: celsius }, output: { celsiusInput: "input" } })
+    ])
+  ]).output((o) => ({
+    fahrenChange: o.fahrenInput.map(getValue),
+    celsiusChange: o.celsiusInput.map(getValue)
+  }));
 
 const main = modelView<Model, View>(model, view)();
 
