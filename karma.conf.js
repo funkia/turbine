d1a2eed7d32eb079ef85e7f9f051f1a02e9a273f
paldepind <simonfv@gmail.com>
2018-08-03 11:11:00 +0200

    Remove PhantomJS and use Chrome headless

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,13 @@
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     frameworks: ["mocha", "karma-typescript"],
-    files: [
-      { pattern: "src/**/*.ts" },
-      { pattern: "test/**/*.ts" }
-    ],
+    files: [{ pattern: "src/**/*.ts" }, { pattern: "test/**/*.ts" }],
     preprocessors: {
       "src/**/*.ts": ["karma-typescript"],
       "test/**/*.ts": ["karma-typescript"]
     },
     reporters: ["mocha", "karma-typescript"],
-    browsers: ["Chrome"],
+    browsers: ["ChromiumHeadless"],
     karmaTypescriptConfig: {
       tsconfig: "tsconfig.json",
       reports: {
