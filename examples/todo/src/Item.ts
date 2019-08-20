044819597ea3702827f1376aa1e46b17266c5914
Simon Friis Vindum <simonfv@gmail.com>
2019-08-20 14:36:38 +0200

    Refactor TodoMVC to use component function

--- examples/todo/src/Item.ts
+++ examples/todo/src/Item.ts
@@ -1,186 +1,126 @@
 import { combine } from "@funkia/jabz";
-import {
-  Behavior,
-  changes,
-  filter,
-  keepWhen,
-  performStream,
-  sample,
-  snapshot,
-  stepper,
-  Stream,
-  lift,
-  toggle
-} from "@funkia/hareactive";
-
-import { modelView, elements, fgo, Component } from "../../../src";
+import * as H from "@funkia/hareactive";
+import { elements, fgo, component } from "../../../src";
 const { div, li, input, label, button, checkbox } = elements;
 
 import { setItemIO, itemBehavior, removeItemIO } from "./localstorage";
 
 const enter = 13;
 const esc = 27;
-const isKey = (keyCode: number) => (ev: { keyCode: number }) =>
-  ev.keyCode === keyCode;
-export const itemIdToPersistKey = (id: number) => `todoItem:${id}`;
-export const itemOutputToId = ({ id }: Output) => id;
 
-export type Item = {
-  taskName: Behavior<string>;
-  isComplete: Behavior<boolean>;
-};
-
-export type PersistedItem = {
-  taskName: string;
-  isComplete: boolean;
-};
-
-export type Input = {
+export type Props = {
   name: string;
   id: number;
-  toggleAll: Stream<boolean>;
-  currentFilter: Behavior<string>;
+  toggleAll: H.Stream<boolean>;
+  currentFilter: H.Behavior<string>;
 };
 
 type FromView = {
-  toggleTodo: Stream<boolean>;
-  taskName: Behavior<string>;
-  startEditing: Stream<any>;
-  nameBlur: Stream<any>;
-  deleteClicked: Stream<any>;
-  nameKeyup: Stream<any>;
-  newNameInput: Stream<any>;
+  toggleTodo: H.Stream<boolean>;
+  taskName: H.Behavior<string>;
+  startEditing: H.Stream<any>;
+  nameBlur: H.Stream<any>;
+  deleteClicked: H.Stream<any>;
+  cancel: H.Stream<any>;
+  enter: H.Stream<any>;
+  newNameInput: H.Stream<any>;
 };
 
 export type Output = {
-  taskName: Behavior<string>;
-  isComplete: Behavior<boolean>;
-  newName: Behavior<string>;
-  isEditing: Behavior<boolean>;
-  focusInput: Stream<any>;
-  hidden: Behavior<boolean>;
-  destroyItemId: Stream<number>;
-  completed: Behavior<boolean>;
+  destroyItemId: H.Stream<number>;
+  completed: H.Behavior<boolean>;
   id: number;
 };
 
-const itemModel = fgo(function*(
-  {
-    toggleTodo,
-    startEditing,
-    nameBlur,
-    deleteClicked,
-    nameKeyup,
-    newNameInput,
-    taskName
-  }: FromView,
-  { toggleAll, name: initialName, id, currentFilter }: Input
-): any {
-  const enterPress = filter(isKey(enter), nameKeyup);
-  const enterNotPressed = yield toggle(true, startEditing, enterPress);
-  const cancel = filter(isKey(esc), nameKeyup);
-  const notCancelled = yield toggle(true, startEditing, cancel);
-  const stopEditing = combine(
-    enterPress,
-    keepWhen(nameBlur, enterNotPressed),
-    cancel
+export default (props: Props) =>
+  component<FromView, Output>(
+    fgo(function*(on) {
+      const enterNotPressed = yield H.toggle(true, on.startEditing, on.enter);
+      const notCancelled = yield H.toggle(true, on.startEditing, on.cancel);
+      const stopEditing = combine(
+        on.enter,
+        H.keepWhen(on.nameBlur, enterNotPressed),
+        on.cancel
+      );
+      const editing = yield H.toggle(false, on.startEditing, stopEditing);
+      const newName = yield H.stepper(
+        props.name,
+        combine(
+          on.newNameInput.map((ev) => ev.target.value),
+          H.snapshot(on.taskName, on.cancel)
+        )
+      );
+      const nameChange = H.snapshot(
+        newName,
+        H.keepWhen(stopEditing, notCancelled)
+      );
+
+      // Restore potentially persisted todo item
+      const persistKey = "todoItem:" + props.id;
+      const savedItem = yield H.sample(itemBehavior(persistKey));
+      const initial =
+        savedItem === null
+          ? { taskName: props.name, completed: false }
+          : savedItem;
+
+      // Initialize task to restored values
+      const taskName: H.Behavior<string> = yield H.stepper(
+        initial.taskName,
+        nameChange
+      );
+      const completed: H.Behavior<boolean> = yield H.stepper(
+        initial.completed,
+        combine(on.toggleTodo, props.toggleAll)
+      );
+
+      // Persist todo item
+      const item = H.lift(
+        (taskName, completed) => ({ taskName, completed }),
+        taskName,
+        completed
+      );
+      yield H.performStream(
+        H.changes(item).map((i) => setItemIO(persistKey, i))
+      );
+
+      const destroyItem = combine(
+        on.deleteClicked,
+        nameChange.filter((s) => s === "")
+      );
+      const destroyItemId = destroyItem.mapTo(props.id);
+
+      // Remove persist todo item
+      yield H.performStream(destroyItem.mapTo(removeItemIO(persistKey)));
+
+      const hidden = H.lift(
+        (complete, filter) =>
+          (filter === "completed" && !complete) ||
+          (filter === "active" && complete),
+        completed,
+        props.currentFilter
+      );
+
+      return li({ class: ["todo", { completed, editing, hidden }] }, [
+        div({ class: "view" }, [
+          checkbox({
+            class: "toggle",
+            props: { checked: completed }
+          }).output({ toggleTodo: "checkedChange" }),
+          label(taskName).output({ startEditing: "dblclick" }),
+          button({ class: "destroy" }).output({ deleteClicked: "click" })
+        ]),
+        input({
+          class: "edit",
+          value: taskName,
+          actions: { focus: on.startEditing }
+        }).output((o) => ({
+          newNameInput: o.input,
+          nameBlur: o.blur,
+          enter: o.keyup.filter((ev) => ev.keyCode === enter),
+          cancel: o.keyup.filter((ev) => ev.keyCode === esc)
+        }))
+      ])
+        .output(() => ({ taskName }))
+        .result({ destroyItemId, completed, id: props.id });
+    })
   );
-  const isEditing = yield toggle(false, startEditing, stopEditing);
-  const newName = yield stepper(
-    initialName,
-    combine(
-      newNameInput.map((ev) => ev.target.value),
-      snapshot(taskName, cancel)
-    )
-  );
-  const nameChange = snapshot(newName, keepWhen(stopEditing, notCancelled));
-
-  // Restore potentially persisted todo item
-  const persistKey = itemIdToPersistKey(id);
-  const savedItem = yield sample(itemBehavior(persistKey));
-  const initial =
-    savedItem === null
-      ? { taskName: initialName, isComplete: false }
-      : savedItem;
-
-  // Initialize task to restored values
-  const taskName_: Behavior<string> = yield stepper(
-    initial.taskName,
-    nameChange
-  );
-  const isComplete: Behavior<boolean> = yield stepper(
-    initial.isComplete,
-    combine(toggleTodo, toggleAll)
-  );
-
-  // Persist todo item
-  const item = lift(
-    (taskName, isComplete) => ({ taskName, isComplete }),
-    taskName_,
-    isComplete
-  );
-  yield performStream(
-    changes(item).map((i: PersistedItem) => setItemIO(persistKey, i))
-  );
-
-  const destroyItem = combine(
-    deleteClicked,
-    nameChange.filter((s) => s === "")
-  );
-  const destroyItemId = destroyItem.mapTo(id);
-
-  // Remove persist todo item
-  yield performStream(destroyItem.mapTo(removeItemIO(persistKey)));
-
-  const hidden = lift(
-    (complete, filter) =>
-      (filter === "completed" && !complete) ||
-      (filter === "active" && complete),
-    isComplete,
-    currentFilter
-  );
-
-  return {
-    taskName: taskName_,
-    isComplete,
-    isEditing,
-    newName,
-    focusInput: startEditing,
-    id,
-    destroyItemId,
-    completed: isComplete,
-    hidden
-  };
-});
-
-function itemView(
-  { taskName, isComplete, isEditing, focusInput, hidden }: Output,
-  _: Input
-): Component<any, FromView> {
-  return li(
-    {
-      class: ["todo", { completed: isComplete, editing: isEditing, hidden }]
-    },
-    [
-      div({ class: "view" }, [
-        checkbox({
-          class: "toggle",
-          props: { checked: isComplete }
-        }).output({ toggleTodo: "checkedChange" }),
-        label(taskName).output({ startEditing: "dblclick" }),
-        button({ class: "destroy" }).output({ deleteClicked: "click" })
-      ]),
-      input({
-        class: "edit",
-        props: { value: taskName },
-        actions: { focus: focusInput }
-      }).output({
-        newNameInput: "input",
-        nameKeyup: "keyup",
-        nameBlur: "blur"
-      })
-    ]
-  ).output((o) => ({ taskName, ...o }));
-}
-
-export default modelView(itemModel, itemView);
--- examples/todo/src/TodoApp.ts
+++ examples/todo/src/TodoApp.ts
@@ -1,194 +1,159 @@
 import { fgo, sequence, IO, combine } from "@funkia/jabz";
-import {
-  Behavior,
-  sample,
-  snapshot,
-  Stream,
-  performStream,
-  changes,
-  lift,
-  snapshotWith,
-  accumCombine,
-  moment,
-  shiftCurrent,
-  empty
-} from "@funkia/hareactive";
-import { modelView, elements, list, output } from "../../../src";
+import * as H from "@funkia/hareactive";
+import { elements, list, component } from "../../../src";
 const { h1, p, header, footer, section, checkbox, ul, label } = elements;
-import { Router, routePath } from "@funkia/rudolph";
+import { locationHashB } from "@funkia/rudolph";
 
 import todoInput, { Out as InputOut } from "./TodoInput";
 import item, {
   Output as ItemOut,
-  Input as ItemParams,
+  Props as ItemParams,
   itemIdToPersistKey
 } from "./Item";
-import todoFooter, { Params as FooterParams } from "./TodoFooter";
+import todoFooter from "./TodoFooter";
 import { setItemIO, itemBehavior, removeItemIO } from "./localstorage";
 
 const isEmpty = (array: any[]) => array.length === 0;
 const includes = <A>(a: A, list: A[]) => list.indexOf(a) !== -1;
 
 type FromView = {
-  toggleAll: Stream<boolean>;
-  itemOutputs: Behavior<ItemOut[]>;
-  clearCompleted: Stream<{}>;
+  toggleAll: H.Stream<boolean>;
+  itemOutputs: H.Behavior<ItemOut[]>;
+  clearCompleted: H.Stream<{}>;
 } & InputOut;
-
-type ToView = {
-  toggleAll: Stream<boolean>;
-  todoNames: Behavior<ItemParams[]>;
-  itemOutputs: Behavior<ItemOut[]>;
-  areAllCompleted: Behavior<boolean>;
-} & FooterParams;
 
 // A behavior representing the current value of the localStorage property
 const todoListStorage = itemBehavior("todoList");
 
-function getCompletedIds(outputs: Behavior<ItemOut[]>): Behavior<number[]> {
-  return moment((at) => {
+const getCompletedIds = (outputs: H.Behavior<ItemOut[]>) =>
+  H.moment((at) => {
     return at(outputs)
       .filter((o) => at(o.completed))
       .map((o) => o.id);
   });
-}
 
 type ListModel<A, B> = {
-  prependItemS: Stream<A>;
-  removeKeyListS: Stream<B[]>;
+  prependItemS: H.Stream<A>;
+  removeKeyListS: H.Stream<B[]>;
   itemToKey: (a: A) => B;
   initial: A[];
 };
 
 // This model handles the modification of the list of Todos
-function listModel<A, B>({
-  prependItemS,
-  removeKeyListS,
-  itemToKey,
-  initial
-}: ListModel<A, B>) {
-  return accumCombine(
+function listModel<A, B>(props: ListModel<A, B>) {
+  return H.accumCombine(
     [
-      [prependItemS, (item, list) => [item].concat(list)],
+      [props.prependItemS, (item, list) => [item].concat(list)],
       [
-        removeKeyListS,
-        (keys, list) => list.filter((item) => !includes(itemToKey(item), keys))
+        props.removeKeyListS,
+        (keys, list) =>
+          list.filter((item) => !includes(props.itemToKey(item), keys))
       ]
     ],
-    initial
+    props.initial
   );
 }
 
-function* model({ addItem, toggleAll, clearCompleted, itemOutputs }: FromView) {
-  const nextId = itemOutputs.map(
-    (outs) => outs.reduce((maxId, { id }) => Math.max(maxId, id), 0) + 1
-  );
+export const app = component<FromView>(
+  fgo(function*(on) {
+    const nextId = on.itemOutputs.map(
+      (outs) => outs.reduce((maxId, { id }) => Math.max(maxId, id), 0) + 1
+    );
 
-  const newTodoS = snapshotWith((name, id) => ({ name, id }), nextId, addItem);
-  const deleteS = shiftCurrent(
-    itemOutputs.map((list) =>
-      list.length > 0 ? combine(...list.map((o) => o.destroyItemId)) : empty
-    )
-  );
-  const completedIds = getCompletedIds(itemOutputs);
-
-  const savedTodoName: ItemParams[] = yield sample(todoListStorage);
-  const restoredTodoName = savedTodoName === null ? [] : savedTodoName;
-
-  const clearCompletedIdS = snapshot(completedIds, clearCompleted);
-  const removeListS = combine(deleteS.map((a) => [a]), clearCompletedIdS);
-  const todoNames = yield listModel<{ id: number; name: string }, number>({
-    prependItemS: newTodoS,
-    removeKeyListS: removeListS,
-    itemToKey: ({ id }) => id,
-    initial: restoredTodoName
-  });
-
-  yield performStream(
-    clearCompletedIdS.map((ids) =>
-      sequence(IO, ids.map((id) => removeItemIO(itemIdToPersistKey(id))))
-    )
-  );
-  yield performStream(changes(todoNames).map((n) => setItemIO("todoList", n)));
-
-  const areAllCompleted = lift(
-    (currentIds, currentOuts) => currentIds.length === currentOuts.length,
-    completedIds,
-    itemOutputs
-  );
-  const areAnyCompleted = completedIds.map(isEmpty).map((b) => !b);
-
-  return {
-    itemOutputs,
-    todoNames,
-    clearAll: clearCompleted,
-    areAnyCompleted,
-    toggleAll,
-    areAllCompleted
-  };
-}
-
-function view(
-  {
-    itemOutputs,
-    todoNames,
-    areAnyCompleted,
-    toggleAll,
-    areAllCompleted
-  }: ToView,
-  router: Router
-) {
-  const currentFilter = routePath(
-    {
-      "/active": () => "active",
-      "/completed": () => "completed",
-      "*": () => ""
-    },
-    router
-  );
-  return [
-    section({ class: "todoapp" }, [
-      header({ class: "header" }, [
-        h1("todos"),
-        todoInput.output({ addItem: "addItem" })
-      ]),
-      section(
-        {
-          class: ["main", { hidden: todoNames.map(isEmpty) }]
-        },
-        [
-          checkbox({
-            class: "toggle-all",
-            attrs: { id: "toggle-all" },
-            props: { checked: areAllCompleted }
-          }).output({ toggleAll: "checkedChange" }),
-          label({ attrs: { for: "toggle-all" } }, "Mark all as complete"),
-          ul(
-            { class: "todo-list" },
-            list(
-              (n) =>
-                item({ toggleAll, currentFilter, ...n }).output({
-                  completed: "completed",
-                  destroyItemId: "destroyItemId",
-                  id: "id"
-                }),
-              todoNames,
-              (o) => o.id
-            ).output((o) => ({ itemOutputs: o }))
-          )
-        ]
-      ),
-      output(
-        { clearCompleted: "clearCompleted" },
-        todoFooter({ todosB: itemOutputs, areAnyCompleted, currentFilter })
+    const newTodoS = H.snapshotWith(
+      (name, id) => ({ name, id }),
+      nextId,
+      on.addItem
+    );
+    const deleteS = H.shiftCurrent(
+      on.itemOutputs.map((list) =>
+        list.length > 0 ? combine(...list.map((o) => o.destroyItemId)) : H.empty
       )
-    ]),
-    footer({ class: "info" }, [
-      p("Double-click to edit a todo"),
-      p("Written with Turbine"),
-      p("Part of TodoMVC")
-    ])
-  ];
-}
+    );
+    const completedIds = getCompletedIds(on.itemOutputs);
 
-export const app = modelView<ToView, FromView, Router>(fgo(model), view);
+    const savedTodoName: ItemParams[] = yield H.sample(todoListStorage);
+    const restoredTodoName = savedTodoName === null ? [] : savedTodoName;
+
+    const clearCompletedIdS = H.snapshot(completedIds, on.clearCompleted);
+    const removeListS = combine(deleteS.map((a) => [a]), clearCompletedIdS);
+    const todoNames = yield listModel<{ id: number; name: string }, number>({
+      prependItemS: newTodoS,
+      removeKeyListS: removeListS,
+      itemToKey: ({ id }) => id,
+      initial: restoredTodoName
+    });
+
+    yield H.performStream(
+      clearCompletedIdS.map((ids) =>
+        sequence(IO, ids.map((id) => removeItemIO(itemIdToPersistKey(id))))
+      )
+    );
+    yield H.performStream(
+      H.changes(todoNames).map((n) => setItemIO("todoList", n))
+    );
+
+    const areAllCompleted = H.lift(
+      (a, b) => a.length === b.length,
+      completedIds,
+      on.itemOutputs
+    );
+    const areAnyCompleted = completedIds.map(isEmpty).map((b) => !b);
+
+    // Strip the leading `/` from the hash location
+    const currentFilter = locationHashB.map((s) => s.slice(1));
+    const hidden = todoNames.map(isEmpty);
+
+    const itemsLeft = H.moment(
+      (at) => at(on.itemOutputs).filter((t) => !at(t.completed)).length
+    );
+
+    return [
+      section({ class: "todoapp" }, [
+        header({ class: "header" }, [
+          h1("todos"),
+          todoInput.output({ addItem: "addItem" })
+        ]),
+        section(
+          {
+            class: ["main", { hidden }]
+          },
+          [
+            checkbox({
+              class: "toggle-all",
+              attrs: { id: "toggle-all" },
+              props: { checked: areAllCompleted }
+            }).output({ toggleAll: "checkedChange" }),
+            label({ attrs: { for: "toggle-all" } }, "Mark all as complete"),
+            ul(
+              { class: "todo-list" },
+              list(
+                (n) =>
+                  item({ toggleAll: on.toggleAll, currentFilter, ...n }).output(
+                    {
+                      completed: "completed",
+                      destroyItemId: "destroyItemId",
+                      id: "id"
+                    }
+                  ),
+                todoNames,
+                (o) => o.id
+              ).output((o) => ({ itemOutputs: o }))
+            )
+          ]
+        ),
+        todoFooter({
+          itemsLeft,
+          areAnyCompleted,
+          currentFilter,
+          hidden
+        }).output({ clearCompleted: "clearCompleted" })
+      ]),
+      footer({ class: "info" }, [
+        p("Double-click to edit a todo"),
+        p("Written with Turbine"),
+        p("Part of TodoMVC")
+      ])
+    ];
+  })
+);
